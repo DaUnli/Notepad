578485fa1a858b3f95d1d66235bb425226434d4e
jayHdyi <gilmiercabil@gmail.com>
2026-02-06 15:57:20 +0800

    u

--- Backend/index.js
+++ Backend/index.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://notepadbyunli.onrender.com"],
+    origin: ["http://localhost:5173", "https://notepad-24hm.onrender.com"],
     credentials: true,
   }),
 );
